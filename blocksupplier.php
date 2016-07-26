60528ce0ceeab9e27f5c2781ae4591a3bcde509c
Maxime Biloé <maxime.biloe@gmail.com>
2016-07-26 16:43:15 +0200

    // Version updated to v1.2.1

--- blocksupplier.php
+++ blocksupplier.php
@@ -33,7 +33,7 @@ class BlockSupplier extends Module
     {
         $this->name = 'blocksupplier';
         $this->tab = 'front_office_features';
-        $this->version = '1.2.0';
+        $this->version = '1.2.1';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
